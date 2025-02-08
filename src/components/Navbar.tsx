4eadde4bb60126dd49ab842c79ea803a97af74fd
praveenmahesh06 <praveenmahesh06@gmail.com>
2025-02-08 14:29:39 +0530

    (nav):added fixed navigation and changed logo size

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,50 +1,42 @@
 "use client";
 import Link from "next/link";
 import ThemeChanger from "./DarkSwitch";
-import Image from "next/image"
+import Image from "next/image";
 import { Disclosure } from "@headlessui/react";
-
+import { Montserrat } from 'next/font/google';
+const montserrat = Montserrat({ subsets: ['latin'] });
 export const Navbar = () => {
-  const navigation = [
-    "L3 Squad",
-    "Projects",
-    "Testimonials",
-    "Community",
-    "Blog",
-  ];
+  const navigation = ["L3 Squad", "Projects", "Testimonials", "Community", "Blog"];
 
   return (
     <div className="w-full">
-      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-1">
-        {/* Logo  */}
-        <Link href="/">
-          <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
-              <span>
-                <Image
-                  src="/img/l3squad.jpg"
-                  width="32"
-                  alt="N"
-                  height="32"
-                  className="w-8"
-                />
-              </span>
-            <span>L3 Squad</span>
-          </span>
-        </Link>
-
-        {/* get started  */}
-        <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
+      <nav className="fixed top-0 left-0 w-full bg-white dark:bg-gray-900 z-50">
+        <div className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-1">
+          <Link href="/">
+            <div className="flex flex-col items-center">
+              <Image
+                src="/img/l3squad.jpg"
+                width={100}
+                height={100}
+                alt="N"
+                className="w-24"
+              />
+              <span className={`text-xl font-medium mt-2 text-purple-600 ${montserrat.className}`}>L3 SQUAD</span>
+            </div>
+          </Link>
+          {/* get started */}
+          <div className="gap-3 nav__item mr-2 lg:flex ml-auto lg:ml-0 lg:order-2">
             <ThemeChanger />
             <div className="hidden mr-3 lg:flex nav__item">
               <Link href="/" className="px-6 py-2 text-white bg-indigo-600 rounded-md md:ml-5">
                 Get Started
               </Link>
             </div>
-        </div>
-                
-        <Disclosure>
-          {({ open }) => (
-            <>
+          </div>
+
+          <Disclosure>
+            {({ open }) => (
+              <>
                 <Disclosure.Button
                   aria-label="Toggle Menu"
                   className="px-2 py-1 text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700">
@@ -71,34 +63,38 @@ export const Navbar = () => {
                 <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
                   <>
                     {navigation.map((item, index) => (
-                      <Link key={index} href="/" className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
-                          {item}
+                      <Link
+                        key={index}
+                        href="/"
+                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
+                        {item}
                       </Link>
                     ))}
-                    <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
-                        Get Started
+                    <Link href="/" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">
+                      Get Started
                     </Link>
                   </>
                 </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        
-        {/* menu  */}
-        <div className="hidden text-center lg:flex lg:items-center">
-          <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
-              <li className="mr-3 nav__item" key={index}>
-                <Link href="/" className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
-                    {menu}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+              </>
+            )}
+          </Disclosure>
 
+          {/* menu */}
+          <div className="hidden text-center lg:flex lg:items-center">
+            <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
+              {navigation.map((menu, index) => (
+                <li className="mr-3 nav__item" key={index}>
+                  <Link
+                    href="/"
+                    className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
+                    {menu}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </nav>
     </div>
   );
-}
-
+};
